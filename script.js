daeca82919d35f517b211da93944c3790eb7e206
Karan Dhakad <karandhakad883@gmail.com>
2023-12-06 12:39:04 +0530

    new js code

--- script.js
+++ script.js
@@ -34,23 +34,118 @@
 // a = a+ 1
 //  console.log(++a)
 
-let car = {
-        brand: "volvo",
-        model: 2023,
-        price: 5500000,
 
-        feature: {
-            airBag: true,
+// -----------------------------------------------
 
-        }
+// let car = {
+//         brand: "volvo",
+//         model: 2023,
+//         price: 5500000,
+
+//         feature: {
+//             airBag: true,
+//             speed : 200
+//         }
+// }
+
+// let {brand:company} = car   //Change key name
+//  car['model'] = 2022;       // Change key value
+
+
+//  console.log(company)
+//  console.log(car.model)
+
+// -----------------------------------
+
+
+// let users = [{
+//     name:"ankush",
+//     frnd:[
+//         "ankit","ajay"
+//     ]
+// },{
+//     name:"ankit",
+//     frnd:[
+//         "ayush","ajay1"
+//     ]
+// },{
+//     name:"ayush",
+//     frnd:[
+//         "ajay","ajay2"
+//     ]
+// },{
+//     name:"ajay",
+//     frnd:[
+//         "anjaykit","ajay3"
+//     ]
+// }]
+
+// console.log(users[1].name)
+
+// let [a,b,c,d] = users
+
+// let {name} = a
+
+// console.log(name)
+
+// for(let i =0;i<4;i++)
+// {
+
+//     for(let j=0;j<2;j++)
+//     {
+//         console.log(users[i].frnd[j])
+
+//     }
+// }
+
+
+
+// let car = [
+//     {model: 2018},
+//     {brand:"volvo"},
+//     {feature: "All",
+//      featureList:["digitalScreen","autoWindow"]},
+//     {Price:"$3000"}
+// ]
+
+
+//     for(i=0; i<4; i++)
+//     {
+//         console.log(car[i]);
+//         if(i==2)
+//         {
+//             console.log(car[i].featureList[0]);
+//             console.log(car[i].featureList[1]);
+//         }
+//     }
+//     console.log(car[4]);
+
+
+let users = [
+    {
+        name:"ankush",
+        sname:"saxena"
+    },
+    {
+        name:"dheeraj",
+        sname:"jain"
+    }
+]
+
+
+for(let i=0;i<users.length;i++)
+{
+    let dummy = users[i].name.slice(2, users[i].name.length);
+    let dummys = users[i].sname.slice(users[i].sname.length - 2, users[i].sname.length);
+    
+    let dummy2 = users[i].name.slice(0,2);
+    let dummys2 = users[i].sname.slice(0, users[i].sname.length - 2);
+    
+    let first = dummys + dummy;
+    let second =  dummys2 + dummy2
+
+    console.log(first)
+    console.log(second)
 }
-
-console.log(car)
-
-
-arr = ["hey", "cutie", "pie"]
-
-console.log(arr[1]);
-
 
 
